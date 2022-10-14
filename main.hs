4f3d9de18d0afd82cb84625fcf708567e1eb0330
toni-santos <a.oliveirasantos122@gmail.com>
2022-10-14 13:35:25 +0100

    finished working on parsing the string input

--- main.hs
+++ main.hs
@@ -1,22 +1,45 @@
-{-# LANGUAGE DataKinds #-}
 import Data.List
+import Data.Char
+import Data.String
+import Text.Parsec (parse)
 
-combine :: [a] -> [a] -> [[a]]
-combine xs ys = [xs,ys]
+delim :: (String, [String], String) -> [Char] -> [String]
+delim ([], res, c) a = res ++ [c]
+delim (x:xs, res, c)  a | x == ' ' || x == '\n' || x == '(' || x == ')' = delim (xs, res, c) a
+                        | x == '-' && c /= "" = delim (xs, res ++ [c], "-") a
+                        | x `elem` a = delim (xs, res ++ [c], "") a
+                        | otherwise = delim (xs, res, c ++ [x]) a
 
-delim :: (String, [String], String) -> [String]
-delim ([], res, c) = res ++ [c]
-delim (x:xs, res, c) | x == ' ' || x == '\n' || x == '(' || x == ')' = delim (xs, res, c)
-                       | x == '+' = delim (xs, res ++ [c], "")
-                       | x == '-' && c /= "" = delim (xs, res ++ [c], "-")
-                       | otherwise = delim (xs, res, c ++ [x])
 
-parseMonom :: String -> [String]
-parseMonom i = [takeWhile (/= '^') i]-- `intersect` takeWhile (/= '^') i]--, takeWhile (/= '*') i, dropWhile (/= '^') i];
+intify :: String -> [Int]
+intify str = map read $ words str :: [Int]
 
-monom :: ([String], [String]) -> [String]
-monom ([], res) = res
-monom (x:xs, res) = monom (xs, res parseMonom x)
+findMonomCoef :: String -> Int
+findMonomCoef str =  if str == "-" then -1 else read (takeWhile (\x -> isDigit x || x == '-') str)
 
-parseInput :: String -> [String]
-parseInput input = monom (delim (input, [], ""), [])
+findMonomExps :: String -> [Char]
+findMonomExps str = filter (\x -> isDigit x && x /= '*' && x /= '^') (dropWhile (\x -> isDigit x || x == '-') str)
+
+findMonomVars :: String -> [Char]
+findMonomVars = filter (\x -> isAlpha x && x /= '*' && x /= '^')
+
+findMonomLit :: String -> String
+findMonomLit = dropWhile (\x -> isDigit x || x == '-' || x == '*')
+
+parseMonomLit :: (String, [(Char, Int)]) -> [(Char, Int)] -- x2y2 x xy2 x2y xy
+parseMonomLit ([], res) = res
+parseMonomLit (str, res) = parseMonomLit (dropWhile (\x -> isDigit x || x == '^' || x == '*') (tail str), res ++ [(head str, read (
+    if null (takeWhile isDigit (dropWhile (\x -> isAlpha x || x == '^' || x == '*') str)) then "1" else takeWhile isDigit (dropWhile (\x -> isAlpha x || x == '^' || x == '*') str)
+    ))])
+
+parseMonom :: String -> (Int, [(Char, Int)])
+parseMonom str = (findMonomCoef str, if null (parseMonomLit (findMonomLit str, [])) then [('_', 0)] else parseMonomLit (findMonomLit str, []))
+
+parsePolis :: [String] -> [(Int, [(Char, Int)])]
+parsePolis = map parseMonom
+
+parseStr :: String -> [String]
+parseStr i = delim (i, [], "") ['+']
+
+parseInput :: String -> [(Int, [(Char, Int)])]
+parseInput input = parsePolis (parseStr input)
